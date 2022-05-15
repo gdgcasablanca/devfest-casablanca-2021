d842ed1b2e30e81bd2d90521f49b1adb085e74d8
Omar Houmz <omarhoumz@gmail.com>
2022-05-15 22:09:55 +0100

    feat: favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,14 +4,14 @@ import HomeScreen from '../src/screens/home'
 
 export default function Home() {
   return (
-    <div className='bg-[#EFF7FF] min-h-screen'>
+    <div className='min-h-screen'>
       <Head>
         <title>Devfest Casablanca 2021 - GDG Casablanca</title>
         <meta
           name='description'
           content='GDG Casablanca organizes a DevFest Conference every year.'
         />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' type='image/png' href='/gdg-casa-icon.png' />
       </Head>
 
       <HomeScreen />
